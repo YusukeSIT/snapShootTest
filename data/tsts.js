4f9a90af9173a18c5bbc156d589438731e5050cf
YusukeSIT <151612445+YusukeSIT@users.noreply.github.com>
2024-09-26 09:37:18 +0900

    Add files via upload

--- data/tsts.js
+++ data/tsts.js
@@ -1,35 +1,4 @@
-let new_assets;
-let new_assets_item;
-let new_gltf_model;
-var bool = false;
 
-new_assets = document.createElement('a-assets');
-
-new_assets_item = document.createElement('a-assets-item');
-new_assets_item.setAttribute('id', 'model-frame');
-new_assets_item.setAttribute('src', './data/frame.glb');
-
-new_assets.appendChild(new_assets_item);
-document.querySelector('a-scene').appendChild(new_assets);
-
-//　木枠の生成
-new_gltf_model = document.createElement('a-gltf-model');
-new_gltf_model.setAttribute('id', 'frame');
-new_gltf_model.setAttribute('class', 'device');
-//new_gltf_model.setAttribute('dynamic-body', 'shape: cube; mass: 1');
-new_gltf_model.setAttribute('position', '0 -5 -5');
-new_gltf_model.setAttribute('rotation', '0 0 0');
-new_gltf_model.setAttribute('scale', '0.6 0.6 0.6')
-new_gltf_model.setAttribute('src', '#model-frame');
-new_gltf_model.setAttribute('box-colider', '');
-
-document.querySelector('a-scene').appendChild(new_gltf_model);
-
-new_assets_item = document.createElement('a-assets-item');
-new_assets_item.setAttribute('id', 'model-tube');
-new_assets_item.setAttribute('src', './data/tube.glb');
-
-new_assets.appendChild(new_assets_item);
 
 
 
